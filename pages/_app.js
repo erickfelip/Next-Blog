9efba8404ee99c36a543734e6308fe5308fed0d5
Erick Felipe <erickfelipe2010@gmail.com>
2022-04-14 16:35:43 -0300

    feat: Add aos library

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,20 @@
 import { NavBar } from "../components/NavBar";
 import { Footer } from "../components/Footer";
+import React, { useEffect } from "react";
+import AOS from "aos";
+
+import "aos/dist/aos.css";
 import initFontAwesome from "../styles/initFontAwesome";
 import "../styles/index.scss";
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    AOS.init({ duration: 2000 });
+  }, []);
   return (
-    <div className="wrapper">
+    <div className="wrapper" >
       <NavBar />
-      <div className="container">
+      <div className="container" data-aos="fade-left">
         <Component {...pageProps} />
       </div>
       <Footer />
