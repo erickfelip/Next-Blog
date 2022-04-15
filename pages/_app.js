b47dfb77fe602436bdffd7f670122c07c4185654
erickfelip <erickfelipe2010@gmail.com>
2022-04-15 19:21:56 -0300

    refactor: Remove aos library

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,14 @@
 import { NavBar } from "../components/NavBar";
 import { Footer } from "../components/Footer";
-import React, { useEffect } from "react";
+import React from "react";
 import initFontAwesome from "../styles/initFontAwesome";
 import "../styles/index.scss";
-import AOS from "aos";
-import "aos/dist/aos.css";
 
 function MyApp({ Component, pageProps }) {
-  useEffect(() => {
-    AOS.init({ duration: 2000 });
-  }, []);
   return (
     <div className="wrapper">
       <NavBar />
-      <div className="container" data-aos="fade-left">
+      <div className="container">
         <Component {...pageProps} />
       </div>
       <Footer />
