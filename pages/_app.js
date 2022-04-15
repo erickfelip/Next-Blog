fd2975305a0b35df96568fbe7e35803adacaacee
erickfelip <erickfelipe2010@gmail.com>
2022-04-15 19:16:00 -0300

    feat: Add aos transition
    -Install aos library

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,19 @@
 import { NavBar } from "../components/NavBar";
 import { Footer } from "../components/Footer";
+import React, { useEffect } from "react";
 import initFontAwesome from "../styles/initFontAwesome";
 import "../styles/index.scss";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    AOS.init({ duration: 2000 });
+  }, []);
   return (
     <div className="wrapper">
       <NavBar />
-      <div className="container">
+      <div className="container" data-aos="fade-left">
         <Component {...pageProps} />
       </div>
       <Footer />
